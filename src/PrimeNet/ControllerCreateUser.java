e12873b43763f5b4c90faaeeb8579e68e4904e26
nsr <scherren@dhbw-loerrach.de>
2018-03-04 21:32:42 +0100

    Eigentlich fertig

--- src/PrimeNet/ControllerCreateUser.java
+++ src/PrimeNet/ControllerCreateUser.java
@@ -2,11 +2,9 @@ package PrimeNet;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
